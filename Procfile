59937d4df5b79f35cd8fad9a68b75d20c29492d1
Clint Berry <clint@system-11.com>
2013-10-24 17:23:46 -0600

    Added first function for hoggle and updated travis profile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-app: bin/hubot -a irc -n Hoggle
+app: node hoggle.js
--- hoggle.js
+++ hoggle.js
@@ -1,3 +1,39 @@
 var Domo = require('domo-kun');
 var nconf = require('nconf');
+var http = require('http');
 
+nconf.argv()
+     .env()
+     .file('config.json');
+
+
+var config = {
+   nick: 'hoggle',
+   userName: 'hoggle',
+   realName: 'Hoggle',
+   address: 'irc.freenode.org',
+   channels: ['#utahjs'],
+   users: [
+     {
+       username: nconf.get('botadminuser'),
+       password: nconf.get('botadminpass')
+     }
+   ],
+   debug: true
+};
+
+var domo = new Domo(config);
+domo.route('Hello hoggle', function(res) {
+   this.say(res.channel, 'Well hello there ' + res.nick + '!');
+});
+
+
+domo.connect();
+
+
+// Webserver required for heroku
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Hi. I\'m Hoggle');
+  res.end();
+}).listen( process.env.PORT || 5000);
